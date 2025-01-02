807b2dcecb428db1ed0352a9a79b5b5822f464db
MaximusCrowe <deathblood69002@gmail.com>
2025-01-02 23:31:47 +0100

    test(auth): Suppression tests inutiles

--- src/__tests__/layouts/BaseLayout.test.tsx
+++
@@ -1,8 +0,0 @@
-import {expect, test} from 'vitest'
-import {render} from '@testing-library/react'
-import BaseLayout from '@/layouts/BaseLayout'
-
-test('Layout', () => {
-  const component = render(<BaseLayout>Layout</BaseLayout>)
-  expect(component).toBeDefined()
-})
